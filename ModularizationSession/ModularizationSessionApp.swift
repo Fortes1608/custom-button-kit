96d381c14a1bc48684e4ab7fa1932c6e1d2a654a
Lorenzo Fortes <mrlorenzo1608@gmail.com>
2025-10-24 13:53:22 -0300

    Initial Commit

---
+++ ModularizationSession/ModularizationSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularizationSessionApp.swift
+//  ModularizationSession
+//
+//  Created by Lorenzo Fortes on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ModularizationSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
